9004821b29b30ac1ed7f8bf3a963123652fdbab4
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-05-31 21:09:42 -0300

    criação - projeto- portfólio/Jonathan Meninez-dev

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import "./Menu.css";
+import React from "react";
+
+const Menu = (props) => {
+  return (
+    <div className="Menu">
+      <h1>Menu</h1>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/views/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+import Menu from "../components/Menu";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+};
+
+export default App;
