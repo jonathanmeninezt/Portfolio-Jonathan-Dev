18e196bfafca8073500f9c58b57988fab1a28755
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-06-10 23:35:34 -0300

    ajuste -container-about p/ h1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,19 +7,15 @@ const About = (props) => {
       <div className="container-about">
         <h1>Sobre mim</h1>
         <p>
-          Olá! Sou o Jonathan Meninez, estudante de graduação em Análise e
-          Desenvolvimento de Sistemas, e estou pronto para impulsionar minha
-          carreira como Desenvolvedor Front End. Com mais de 2 anos de
-          experiência como Desenvolvedor Front End Freelance, tenho dominado uma
-          ampla gama de competências em tecnologia, entregando projetos de alto
-          impacto e qualidade. Sou apaixonado por programação e busco
-          constantemente aprimorar minhas habilidades para me destacar no
-          mercado. Minhas competências incluem API, estruturas de dados,
-          desenvolvimento de projetos, linguagens de programação, programação
-          lógica, tecnologia da informação e muito mais. Trabalho com
-          JavaScript, HTML5, CSS, AJAX, React.js, JQuery, SQL e PostgreSQL,
-          aproveitando as melhores práticas de programação e frameworks para
-          web.
+          Sou Desenvolvedor Front End e tenho dominado uma ampla gama de
+          competências em tecnologia, entregando projetos de alto impacto e
+          qualidade. Sou apaixonado por programação e busco constantemente
+          aprimorar minhas habilidades para me destacar no mercado. Minhas
+          competências incluem API, estruturas de dados, desenvolvimento de
+          projetos, linguagens de programação, programação lógica, tecnologia da
+          informação e muito mais. Trabalho com JavaScript, HTML5, CSS, AJAX,
+          React.js, JQuery, SQL e PostgreSQL, aproveitando as melhores práticas
+          de programação e frameworks para web.
         </p>
       </div>
     </div>
