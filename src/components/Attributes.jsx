9b5bf2d8361fb6f0d81b2d91eabceef348044708
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-06-11 23:35:02 -0300

    criação- Attributes/Habilidades

---
+++ src/components/Attributes.jsx
@@ -0,0 +1,15 @@
+import "./Attributes.css";
+import React from "react";
+
+const Attributes = (props) => {
+  return (
+    <div className="Attributes">
+      <h1>Habilidaes</h1>
+      <div className="AttributesImg">
+        <img src="/src/image/javascript.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default Attributes;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -5,6 +5,7 @@ import VideoPlayer from "../VideoPlayer.jsx";
 import Menu from "../components/Menu";
 import Home from "../components/Home";
 import About from "../components/About";
+import Attributes from "../components/Attributes";
 
 const App = (props) => {
   return (
@@ -13,6 +14,7 @@ const App = (props) => {
       <Menu />
       <Home />
       <About />
+      <Attributes />
     </div>
   );
 };
