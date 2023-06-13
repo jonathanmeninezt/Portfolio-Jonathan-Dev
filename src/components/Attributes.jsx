6671586d86d40d2f488a09683fae1f0e9395dfcd
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-06-12 23:43:41 -0300

    criação - logo/habilidades/Atribute-container

--- src/components/Attributes.jsx
+++ src/components/Attributes.jsx
@@ -1,12 +1,24 @@
 import "./Attributes.css";
 import React from "react";
+import logoJs from "../image/javascript.svg";
 
 const Attributes = (props) => {
   return (
     <div className="Attributes">
       <h1>Habilidaes</h1>
-      <div className="AttributesImg">
-        <img src="/src/image/javascript.svg" />
+      <div className="AtributesContainer">
+        <div className="AttributesImgJs">
+          <img className="logoJs" src={logoJs} />
+        </div>
+        <div className="AttributesImgJs">
+          <img className="logoJs" src={logoJs} />
+        </div>
+        <div className="AttributesImgJs">
+          <img className="logoJs" src={logoJs} />
+        </div>
+        <div className="AttributesImgJs">
+          <img className="logoJs" src={logoJs} />
+        </div>
       </div>
     </div>
   );
