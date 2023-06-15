9ef5da78f5a0f1d1b44fda0ced0596808881025a
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-06-14 22:35:59 -0300

    criação - fundo estrela/ atributes

--- src/components/Attributes.jsx
+++ src/components/Attributes.jsx
@@ -1,13 +1,17 @@
 import "./Attributes.css";
 import React from "react";
+
 import logoJs from "../image/javascript.svg";
 import logoHtml from "../image/html.svg";
 import logoCss from "../image/css.svg";
 import logoRect from "../image/react.svg";
+import gifEstrela from "../assets/fundoestrela.gif";
 
 const Attributes = (props) => {
   return (
     <div className="Attributes">
+      <img className="gifEstrela" src={gifEstrela} />
+
       <h1>Habilidaes</h1>
       <div className="AtributesContainer">
         <div className="AttributesImgJs">
