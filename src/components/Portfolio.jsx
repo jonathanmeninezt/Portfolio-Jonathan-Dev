717223ee42bc012791d44166a1e9f87a79678262
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-06-21 23:39:21 -0300

    criação - box-portfolio - boxs

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,6 +7,14 @@ const Portfolio = (props) => {
       <div className="Portfolio">
         <h1>Portfólio</h1>
       </div>
+      <div className="box-portfolio">
+        <div className="box-one"></div>
+        <div className="box-two"></div>
+        <div className="box-three"></div>
+        <div className="box-four"></div>
+        <div className="box-five"></div>
+        <div className="box-six"></div>
+      </div>
     </div>
   );
 };
