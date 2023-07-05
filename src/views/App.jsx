371901725ca61674e81f56f2eb1eb5d93b9a9972
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-07-04 21:32:54 -0300

    criação - PortfolioSlide.jsx - img/disc

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import "./App.css";
+import React from "react";
 
 import VideoPlayer from "../VideoPlayer.jsx";
 import Menu from "../components/Menu";
 import Home from "../components/Home";
 import About from "../components/About";
 import Attributes from "../components/Attributes";
-import Portfolios from "../components/Portfolios.jsx";
+import Portfolio from "../components/Portfolio";
 import Contact from "../components/Contact";
 
 const App = (props) => {
@@ -16,27 +17,10 @@ const App = (props) => {
       <Home />
       <About />
       <Attributes />
-      <LightColor>
-        <Portfolios />
-      </LightColor>
-      <Container />
+      <Portfolio />
       <Contact />
     </div>
   );
 };
 
 export default App;
-
-const Container = styled.div``;
-// const Banner = styled.div`
-//   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
-//   height: 100vh;
-//   @media (max-width: 640px) {
-//     height: 100%;
-//     padding-bottom: 2rem;
-//   }
-// `;
-
-const LightColor = styled.div`
-  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
-`;
