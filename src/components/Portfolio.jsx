f9b1faddb088d3b1e0035a2c5c751b498dde5413
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-07-04 21:06:01 -0300

    criação - Portfolios.jsx - slide - slick

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,65 @@
-import "./Portfolio.css";
 import React from "react";
+import styled from "styled-components";
 
 const Portfolio = (props) => {
+  const { img, disc } = props.item;
   return (
-    <div>
-      <div className="Portfolio">
-        <h1>Portfólio</h1>
+    <Container className="project">
+      <img src={img} alt="project" />
+      <div className="disc">
+        <button>Veja Mais</button>
       </div>
-      <div className="box-portfolio">
-        <div className="box-one">Conteudo 1</div>
-        <div className="box-two">Conteudo 1</div>
-        <div className="box-three">Conteudo 1</div>
-        <div className="box-four">Conteudo 1</div>
-        <div className="box-five">Conteudo 1</div>
-        <div className="box-six">Conteudo 1</div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
 export default Portfolio;
+
+const Container = styled.div`
+  height: 10rem;
+  background-color: #4e5156;
+  margin: 0 0.5rem;
+  padding: 0.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 400ms ease-in-out;
+  }
+  .disc button {
+    width: 23%;
+    position: absolute;
+    right: 0;
+    left: 13rem;
+    bottom: -10rem;
+    border-radius: 15px;
+    color: white;
+    background-color: red;
+    transition: all 400ms ease-in-out;
+
+    h1 {
+      font-size: 1rem;
+    }
+
+    p {
+      width: 90%;
+      font-size: 0.8rem;
+      a {
+        margin-left: 0.4rem;
+        color: red;
+      }
+    }
+  }
+
+  img:hover {
+    transform: scale(1.3);
+  }
+
+  :hover .disc button {
+    bottom: 5rem;
+  }
+`;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,12 +1,11 @@
-import "./App.css";
-import React from "react";
+import styled from "styled-components";
 
 import VideoPlayer from "../VideoPlayer.jsx";
 import Menu from "../components/Menu";
 import Home from "../components/Home";
 import About from "../components/About";
 import Attributes from "../components/Attributes";
-import Portfolio from "../components/Portfolio";
+import Portfolios from "../components/Portfolios.jsx";
 import Contact from "../components/Contact";
 
 const App = (props) => {
@@ -17,10 +16,27 @@ const App = (props) => {
       <Home />
       <About />
       <Attributes />
-      <Portfolio />
+      <LightColor>
+        <Portfolios />
+      </LightColor>
+      <Container />
       <Contact />
     </div>
   );
 };
 
 export default App;
+
+const Container = styled.div``;
+// const Banner = styled.div`
+//   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+//   height: 100vh;
+//   @media (max-width: 640px) {
+//     height: 100%;
+//     padding-bottom: 2rem;
+//   }
+// `;
+
+const LightColor = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`;
